b92ac173732fd88b5551512667812038e218a82e
Nikola Jolovic <nikolajolovic02@gmail.com>
2026-05-16 21:36:38 +0200

    feat: cli config

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -3,13 +3,28 @@ package main
 import (
 	"os"
 
+	cliconfig "oblak/internal/cli/config"
+
 	"github.com/spf13/cobra"
 )
+
+var globalCfg cliconfig.Config
 
 var rootCmd = &cobra.Command{
 	Use:   "oblak",
 	Short: "Oblak CLI: Manage your oblak functions",
 	Long:  `Oblak is a CLI tool for managing and deploying serverless functions to your Oblak instance.`,
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		if cmd.Name() == "configure" {
+			return nil
+		}
+		cfg, err := cliconfig.Load()
+		if err != nil {
+			return err
+		}
+		globalCfg = cfg
+		return nil
+	},
 }
 
 func Execute() {
--- internal/cli/config/config.go
+++ internal/cli/config/config.go
@@ -1,1 +1,67 @@
 package config
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/pelletier/go-toml/v2"
+)
+
+type Profile struct {
+	AuthID    string `toml:"auth_id"`
+	SecretKey string `toml:"secret_key"`
+	Endpoint  string `toml:"endpoint"`
+}
+
+type Config map[string]Profile
+
+func Path() string {
+	home, _ := os.UserHomeDir()
+	return filepath.Join(home, ".oblak", "config.toml")
+}
+
+func Load() (Config, error) {
+	cfg := make(Config)
+	data, err := os.ReadFile(Path())
+	if errors.Is(err, os.ErrNotExist) {
+		return cfg, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return cfg, toml.Unmarshal(data, &cfg)
+}
+
+func (c Config) Save() error {
+	p := Path()
+	if err := os.MkdirAll(filepath.Dir(p), 0700); err != nil {
+		return err
+	}
+	data, err := toml.Marshal(c)
+	if err != nil {
+		return err
+	}
+	tmp := p + ".tmp"
+	if err := os.WriteFile(tmp, data, 0600); err != nil {
+		return err
+	}
+	return os.Rename(tmp, p)
+}
+
+func ActiveProfileName() string {
+	if name := os.Getenv("OBLAK_PROFILE"); name != "" {
+		return name
+	}
+	return "default"
+}
+
+func (c Config) ActiveProfile() (Profile, error) {
+	name := ActiveProfileName()
+	p, ok := c[name]
+	if !ok {
+		return Profile{}, fmt.Errorf("profile %q not found: run `oblak configure`", name)
+	}
+	return p, nil
+}
