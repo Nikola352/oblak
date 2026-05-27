6ae370f37d67ce97b89fc8378fde95fef5d6eb20
Nikola Jolovic <nikolajolovic02@gmail.com>
2026-05-27 16:48:38 +0200

    feat: invocation entity

--- migrations/000003_function_state.up.sql
+++ migrations/000003_function_state.up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE functions
 (
-    function_id    TEXT PRIMARY KEY,
-    user_id        UUID NOT NULL REFERENCES users (user_id),
-    status         TEXT NOT NULL,
-    bucket         TEXT NOT NULL,
-    path           TEXT NOT NULL
+    function_id UUID PRIMARY KEY,
+    user_id     UUID NOT NULL REFERENCES users (user_id),
+    status      TEXT NOT NULL,
+    bucket      TEXT NOT NULL,
+    path        TEXT NOT NULL
 )
